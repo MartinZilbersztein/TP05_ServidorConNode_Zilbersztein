1323ac83f09440e2603940da929e8e3fa903ecde
MartinZilber <48520155@est.ort.edu.ar>
2025-04-25 12:12:40 -0300

    Consigna 1 terminada - Arreglar 2 y realizar extra

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 import express from "express";
 import cors from "cors";
-import time from "./time.js";
+import {ObtenerHora, ObtenerFechaCompleta} from "./time.js";
 const app = express();
 
 app.use(express.json());
 app.get('/', (req, res) => {
-    res.json('Hola');
+    res.json('¡Hola, y bienvenido a un servidor express con Node!');
 });
 app.get('/hora', (req, res) => {
-    res.json('Hola');
+    res.send(ObtenerHora())
+});
+app.get('/fecha-completa', (req, res) => {
+    res.send(time.toDateString() + " " +  time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
 });
 
+//para el extra: middleware
 export default app;
--- src/time.js
+++ src/time.js
@@ -1,2 +1,4 @@
-let hora = new Date();
-export default time;
+const ObtenerHora = () => new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds();
+const ObtenerFechaCompleta = () => new Date().toDateString() + " " +  new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds()
+
+export default {ObtenerHora, ObtenerFechaCompleta};
